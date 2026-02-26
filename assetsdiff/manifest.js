e2584f2f80e69f0a170a377d5407ccd8f7c611d5
QbzKJl2 <adrianbelzak@gmail.com>
2026-02-26 10:48:35 +0100

    s

--- assetsdiff/manifest.js
+++ assetsdiff/manifest.js
@@ -1,11 +1,33 @@
 let webManifest = {
-    "name": "",
-    "short_name": "",
+    "name": "Moja Aplikacja",
+    "short_name": "App",
     "theme_color": "#101317",
     "background_color": "#101317",
-    "display": "standalone"
+    "display": "standalone",
+    "icons": [
+    {
+      "src": "https://i.imgur.com/hpmyNb2.png", 
+      "sizes": "64x64",   
+      "type": "image/png"
+    },
+    {
+      "src": "https://i.imgur.com/hpmyNb2.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "https://i.imgur.com/hpmyNb2.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    },
+    {
+      "src": "https://i.imgur.com/hpmyNb2.png",  
+      "sizes": "512x512",
+      "type": "image/png",
+      "purpose": "any maskable"
+    }
+    ]
 };
-
 let manifestElem = document.createElement('link');
 manifestElem.setAttribute('rel', 'manifest');
 manifestElem.setAttribute('href', 'data:application/manifest+json;base64,' + btoa(JSON.stringify(webManifest)));
